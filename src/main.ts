c054f321f3ebd73fdd608e5fb41ec071b3a9d3e5
Nish Tahir <nishtahir@outlook.com>
2024-06-15 14:33:11 -0400

    Print context json

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
 export async function run(): Promise<void> {
-  console.log(github.context);
+  console.log(JSON.stringify(github.context, null, 2));
   console.log("Success...");
 }
